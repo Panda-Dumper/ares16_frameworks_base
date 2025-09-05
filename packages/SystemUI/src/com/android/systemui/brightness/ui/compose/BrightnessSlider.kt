81e1db83de43fec2be74cdf3c0924a0767db1f66
ChrisCatto <chriscatto@proton.me>
2025-09-05 18:47:14 +0000

    Revert "BrightnessSlider: Increase spacer size to avoid clipping in tracking mode"
    This reverts commit 654765d0ae5057a679ba2fba40b6adde23ea2a46.

--- packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
+++ packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
@@ -360,7 +360,7 @@ fun BrightnessSlider(
         )
 
         if (hasAutoBrightness && showAutoBrightness) {
-            Spacer(modifier = Modifier.width(10.dp))
+            Spacer(modifier = Modifier.width(8.dp))
 
             val coroutineScope = rememberCoroutineScope()
             val autoBrightnessBackgroundColor by animateColorAsState(
