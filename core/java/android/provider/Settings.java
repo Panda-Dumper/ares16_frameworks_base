29dcefc010046f015af74df8295c729f0968b712
priiii08918 <priyanshusinghal0818@proton.me>
2025-09-14 09:08:07 +0000

    [SQUASH] Drop GameSpace
    Revert "GameSpaceManager: cleanup and improvements"
    
    This reverts commit 7d49e76f22e485834c69a8ba5e7f51298884cb7f.
    
    Revert "GameSpaceManager: Rewrite and fix app detection issues"
    
    This reverts commit 2e1aecc6d20b8294d40cdfa66b7c4caf50534710.
    
    Revert "GameSpace: Improvements for detecting foreground app exit"
    
    This reverts commit 64ffb8a55ee5220267e08e1ca097feef37baa3eb.
    
    Revert "base: Fix gamespace on secondary users"
    
    This reverts commit 1b67f515b500059ce7f8f7dd941fabda398348bc.
    
    Revert "base: Fix NPE in GameManagerService"
    
    This reverts commit 8558345d659d4eb900a310cda84fc03a55c36197.
    
    Revert "GameManagerService: Prevent multiple threads from accessing settings observer onChange"
    
    This reverts commit a5183a874e82c708c9367cb2051f31240178d538.
    
    Revert "GameManagerService: Set device_config property on behalf of GameSpace"
    
    This reverts commit 24254b6c983842c378760400b7b86e703c2aa31c.
    
    Revert "SystemUI: Add support for GameSpace"
    
    This reverts commit bcb12d9a837e2a7c860a2396f402940f6d932152.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7013,20 +7013,6 @@ public final class Settings {
         public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
 
         /**
-         * GameSpace: List of added games by user
-         * @hide
-         */
-        @Readable
-        public static final String GAMESPACE_GAME_LIST = "gamespace_game_list";
-
-        /**
-         * GameSpace: Whether fullscreen intent will be suppressed while in game session
-         * @hide
-         */
-        @Readable
-        public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
-
-        /**
          * Current status of whether gestures are locked
          * @hide
          */
@@ -13784,15 +13770,6 @@ public final class Settings {
          * @hide
          */
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
-
-        /**
-         * Our GameSpace can't write to device_config directly [GTS]
-         * Use this as intermediate to pass device_config property
-         * from our GameSpace to com.android.server.app.GameManagerService
-         * so we can set the device_config property from there.
-         * @hide
-         */
-        public static final String GAME_OVERLAY = "game_overlay";
 
         /**
          * Whether to show an overlay in the bottom corner of the screen on copying stuff
