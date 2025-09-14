b8bf7a5f1525223ceab44149d432dd5f8bb14de4
priiii08918 <priyanshusinghal0818@proton.me>
2025-09-14 09:25:09 +0000

    [SQUASH] SystemUI: Drop QS & brightness slider changes
    Revert "SystemUI: Add brightness slider shapes"
    
    This reverts commit c8bccc306d6467abfd14e4439078c1c09517ed3a.
    
    Revert "SystemUI: Add customization to select QS tile shape"
    
    This reverts commit 938c02e79b13bfc75fe717a289edd79ebd9c166b.
    
    Revert "SystemUI: BrightnessSlider: Support non-animated custom brightness icons"
    
    This reverts commit 4e8d8dbd7a18d3d7e0b842c15dc585b5f107c1c3.
    
    Revert "BrightnessSlider: Increase spacer size to avoid clipping in tracking mode"
    
    This reverts commit 654765d0ae5057a679ba2fba40b6adde23ea2a46.
    
    Revert "SystemUI: Simplify brightness slider position and visibility"
    
    This reverts commit 411b3bc057a2716de0cc766c6784760fe255d245.
    
    Revert "SystemUI: Allow to change brightness slider positioning in new compose QS [1/2]"
    
    This reverts commit f781b1b967c337f93560737c623d22444b336228.
    
    Revert "Add toggle to enable brightness slider haptic feedback [1/2]"
    
    This reverts commit c2987480f3b3d47d27c8f4dddbe8b5e9b95eac9c.
    
    Change-Id: Ib2e5d3f9619c7b54415d2f49c48da0d342d08158
    
    Revert "BrightnessSlider: Add tunable for showing brightness icon"
    
    This reverts commit d11401914b7cecae9023728ee94f4b72459668e7.
    
    Revert "SystemUI: Add auto brightness toggle in brightness slider in new compose QS"
    
    This reverts commit e4c346b190ac92ff25ce0c48ee54ad40f11fd2a8.
    
    Change-Id: Ie6b4b37ede1fea6fa5e76f20f06148305dc4ae29
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7231,12 +7231,6 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
-         * Haptic feedback on brightness slider
-         * @hide
-         */
-        public static final String QS_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
-
-        /**
          * Whether to show daily/weekly data usage in the QS footer.
          * @hide
          */
@@ -7247,18 +7241,6 @@ public final class Settings {
          * @hide
          */
         public static final String QS_SHOW_DATA_USAGE_WINDOW = "qs_show_data_usage_window";
-
-        /**
-         * Customize QS tile shape.
-         * @hide
-         */
-        public static final String QS_TILE_SHAPE = "qs_tile_shape";
-
-        /**
-         * Customize Brightness slider shape.
-         * @hide
-         */
-        public static final String QS_BRIGHTNESS_SLIDER_SHAPE = "qs_brightness_slider_shape";
 
         /**
          * @hide
